8d6b84fcd6adf18ec1d9fa5764e256a5d0437910
Tassio Santana <tassiosantana01@gmail.com>
2021-09-30 14:26:23 -0300

    aula97

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,7 +1,11 @@
+import * as Styled from './styles';
+
 function Home() {
   return (
     <div className="App">
-      <h1>Tassio</h1>
+      <Styled.Wrapper>
+        <h1>Tassio</h1>
+      </Styled.Wrapper>
     </div>
   );
 }
