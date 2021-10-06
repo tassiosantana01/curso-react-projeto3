a6969fd2d37b8f29bca97d7e6aca20f4b44c72d7
Tassio Santana <tassiosantana01@gmail.com>
2021-10-06 19:32:33 -0300

    aula113-114

--- src/components/MenuLink/MenuLink.test.jsx
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -25,12 +25,12 @@ it('should render open in a new tab', () => {
 });
 
 it('should render open in a new tab', () => {
-  renderTheme(
+  const { container } = renderTheme(
     <MenuLink link="http://localhost" newTab={false}>
       Children
     </MenuLink>,
   );
-  expect(screen.getByRole('link', { name: 'Children' })).toMatchInlineSnapshot(`
+  expect(container.firstChild).toMatchInlineSnapshot(`
     .c0 {
       display: block;
       -webkit-text-decoration: none;
