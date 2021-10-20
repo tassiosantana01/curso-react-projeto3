d9d6ccf02a01b3e854466af8751d4a24da13bfc0
Tassio Santana <tassiosantana01@gmail.com>
2021-10-19 22:11:54 -0300

    aula134

--- src/api/map-data.test.js
+++ src/api/map-data.test.js
@@ -2,12 +2,10 @@ import { mapData } from './map-data';
 
 describe('map-data', () => {
   it('should map data even if there is no data', () => {
-    const pagesData = mapData();
+    const pagesData = mapData()[0];
     expect(pagesData.Html).toBe('');
     expect(pagesData.slug).toBe('');
     expect(pagesData.title).toBe('');
-    expect(pagesData.sections).toEqual([]);
-    expect(pagesData.menu).toEqual({});
   });
 
   it('should map data if there are data', () => {
@@ -16,14 +14,10 @@ describe('map-data', () => {
         footer_text: '<p>Hey</p>',
         slug: 'slug',
         title: 'title',
-        sections: [1, 2, 3, 4],
-        menu: { a: 'b' },
       },
     ])[0];
     expect(pagesData.Html).toBe('<p>Hey</p>');
     expect(pagesData.slug).toBe('slug');
     expect(pagesData.title).toBe('title');
-    expect(pagesData.sections).toEqual([1, 2, 3, 4]);
-    expect(pagesData.menu).toEqual({ a: 'b' });
   });
 });
