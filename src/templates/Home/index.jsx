8ff0351a8ed81877cb032e2c1096ea2b21c26734
Tassio Santana <tassiosantana01@gmail.com>
2021-10-21 16:13:02 -0300

    aula143

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Heading } from '../../components/Heading';
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
 import * as Styled from './styles';
 import { mapData } from '../../api/map-data';
-import { useState } from 'react';
 import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -14,7 +14,7 @@ function Home() {
     const load = async () => {
       try {
         const data = await fetch(
-          'http://localhosta:1337/pages/?slug=landing-page',
+          'http://localhost:1337/pages/?slug=landing-page',
         );
         const json = await data.json();
         const pageData = mapData(json);
@@ -32,7 +32,7 @@ function Home() {
   }
 
   if (data && !data.slug) {
-    return <h1>Carregando...</h1>;
+    return <Loading />;
   }
 
   return <Base {...mockBase} />;
